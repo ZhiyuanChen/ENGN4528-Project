36e9dee2ff79dd7521a25d296f2d7b44d3883a15
Zhiyuan Chen <zc@int.ac.cn>
2019-05-18 20:10:21 +1000

    Load pretrained model

--- lane/master.py
+++ lane/master.py
@@ -1,27 +1,40 @@
+import tensorflow as tf
+
 from globals import Master, Vernie, traceback, load_message, load_image, LANE_REQUEST
 
 
 class LaneMaster(Master):
-    def __init__(self):
+    def __init__(self, model):
         super(LaneMaster, self).__init__(LANE_REQUEST)
-        self.mq.channel.basic_consume(queue=LANE_REQUEST, on_message_callback=self.receive)
+        self.mq.channel.basic_consume(queue=LANE_REQUEST, on_message_callback=self.process)
         self.mq.channel.start_consuming()
+        self.model = model
 
-    def receive(self, ch, method, props, body):
-        self.log.info('Received message from ' + method.routing_key)
+    def receive(self, message):
         try:
-            code, message, data = load_message(body)
+            code, message, data = load_message(message)
             if code != 200:
                 raise Vernie(300, 'Illegal message')
-            image = load_image(data)
+            return load_image(data)
         except Vernie:
             raise Vernie(300, 'Illegal message')
         except Exception:
             raise Vernie(301, 'Failed to receive message', traceback.format_exc())
 
+    def process(self, ch, method, props, body):
+        self.log.info(method.routing_key + ' received ' + props.correlation_id)
+        image = self.receive(body)
+
+
+class Lane(object):
+    def __init__(self):
+        self.model = tf.train.import_meta_graph('./model/model.meta')
+        with tf.Session() as session:
+            self.model.restore(session, tf.train.latest_checkpoint('./model/model'))
+
 
 def main():
-    master = LaneMaster()
+    master = LaneMaster(Lane())
 
 
 if __name__ == "__main__":
