1a2d1e897b62ea418f09232c06439ebef21e8662
Zhiyuan Chen <zc@int.ac.cn>
2019-05-20 23:44:35 +1000

    Update project structure

---
+++ obstacle/__init__.py
@@ -0,0 +1,29 @@
+from obstacle.mrcnn import coco
+from obstacle.mrcnn import utils
+import obstacle.mrcnn.model as modellib
+from obstacle.mrcnn import visualize
+from obstacle.mrcnn.model import log
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
--- obstacle/master.py
+++ obstacle/master.py
@@ -1,15 +1,29 @@
-from globals import Master, Vernie, traceback, load_image, MQ
+import tensorflow as tf
+
+from obstacle import *
+from globals import Master, Vernie, traceback, load_image, MQ, NN, LOG, cv2
 
 
 class ObstMaster(Master):
     def __init__(self):
         super(ObstMaster, self).__init__(MQ.OBST_REQUEST)
+        config = InferenceConfig()
+        with tf.device("/gpu:0"):
+            self.model = modellib.MaskRCNN(mode="inference", model_dir=LOG.DIR, config=config)
+        self.model.load_weights(NN.OBST_WEIGHTS_PATH, by_name=True,
+                           exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
         self.mq.channel.basic_consume(queue=MQ.OBST_REQUEST, on_message_callback=self.process)
         self.mq.channel.start_consuming()
 
     def process(self, ch, method, props, body):
         self.log.info(method.routing_key + ' received ' + props.correlation_id)
         image = load_image(body)
+        result = self.model.detect([image], verbose=1)[0]
+
+
+class Obstacle(object):
+    def __init__(self):
+        pass
 
 
 def main():
