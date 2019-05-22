3904ee126b0d46787fea1024e5e0fe4e9c5d1ca6
Zhiyuan Chen <zc@int.ac.cn>
2019-05-23 01:26:24 +1000

    Update master function to display result

--- master.py
+++ master.py
@@ -1,30 +1,62 @@
-from globals import Master, Vernie, traceback, load_message, load_image, COMP_REQUEST
+from globals import Master, load_image, MQ, cv2, time
 
 
-class CompMaster(Master):
+class TheMaster(Master):
     def __init__(self):
-        super(CompMaster, self).__init__(COMP_REQUEST)
-        self.mq.channel.basic_consume(queue=COMP_REQUEST, on_message_callback=self.process)
+        super(TheMaster, self).__init__()
+        self.mq.channel.basic_consume(queue=MQ.COMP_RESPONSE, on_message_callback=self.comp_process)
+        self.mq.channel.basic_consume(queue=MQ.LANE_RESPONSE, on_message_callback=self.lane_process)
+        self.mq.channel.basic_consume(queue=MQ.OBST_RESPONSE, on_message_callback=self.obst_process)
+        self.mq.channel.basic_consume(queue=MQ.SIGN_RESPONSE, on_message_callback=self.sign_process)
+        self.comp_window = cv2.namedWindow('Comprehensive', 0)
+        self.line_window = cv2.namedWindow('Lane Line', 0)
+        self.obst_window = cv2.namedWindow('Obstacle', 0)
+        self.sign_window = cv2.namedWindow('Traffic Sign', 0)
         self.mq.channel.start_consuming()
 
-    def receive(self, message):
+    def comp_process(self, ch, method, props, body):
         try:
-            code, message, data = load_message(message)
-            if code != 200:
-                raise Vernie(300, 'Illegal message')
-            return load_image(data)
-        except Vernie:
-            raise Vernie(300, 'Illegal message')
-        except Exception:
-            raise Vernie(301, 'Failed to receive message', traceback.format_exc())
+            self.log.info(method.routing_key + ' received message')
+            image = load_image(body)
+            cv2.imshow(self.comp_window, image)
+            cv2.waitKey(1)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception as err:
+            self.log.error(err)
 
-    def process(self, ch, method, props, body):
-        self.log.info(method.routing_key + ' received ' + props.correlation_id)
-        image = self.receive(body)
+    def lane_process(self, ch, method, props, body):
+        try:
+            self.log.info(method.routing_key + ' received message')
+            image = load_image(body)
+            cv2.imshow(self.line_window, image)
+            cv2.waitKey(1)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception as err:
+            self.log.error(err)
+
+    def obst_process(self, ch, method, props, body):
+        try:
+            self.log.info(method.routing_key + ' received message')
+            image = load_image(body)
+            cv2.imshow(self.obst_window, image)
+            cv2.waitKey(1)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception as err:
+            self.log.error(err)
+
+    def sign_process(self, ch, method, props, body):
+        try:
+            self.log.info(method.routing_key + ' received message')
+            image = load_image(body)
+            cv2.imshow(self.sign_window, image)
+            cv2.waitKey(1)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception as err:
+            self.log.error(err)
 
 
 def main():
-    master = CompMaster()
+    master = TheMaster()
 
 
 if __name__ == "__main__":
